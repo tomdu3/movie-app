719a07083150744de64463bc32fb78dc93ce1a04
Tomislav Dukez <tomdu3@ymail.com>
2025-02-25 13:51:55 +0000

    update App with new SearchMovie component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import SearchMovie from './components/SearchMovie'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <h1>Movie Database App</h1>
+     <h1>Tom's Movie Database</h1>
+     <SearchMovie />
     </>
   )
 }
