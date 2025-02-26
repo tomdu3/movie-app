3664468536528f978c81276bf35aa73b7f35cbb6
Tomislav Dukez <tomdu3@ymail.com>
2025-02-26 14:57:52 +0000

    add Movie Details component

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieDetails } from '../services/api';
+
+const MovieDetails = () => {
+  const { imdbID } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const data = await getMovieDetails(imdbID);
+        if (data.Response === 'True') {
+          setMovie(data);
+        } else {
+          setError(data.Error);
+        }
+      } catch (error) {
+        setError('An error occurred while fetching movie details.');
+      }
+    };
+
+    fetchMovieDetails();
+  }, [imdbID]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  const { Title, Year, Poster, Genre, Plot, Actors, Ratings } = movie;
+
+  return (
+    <div>
+      <h2>{Title}</h2>
+      <p>Year: {Year}</p>
+      <img src={Poster} alt={Title} />
+      <p>Genre: {Genre}</p>
+      <p>Plot: {Plot}</p>
+      <p>Actors: {Actors}</p>
+      <h3>Ratings:</h3>
+      <ul>
+        {Ratings.map((rating, index) => (
+          <li key={index}>
+            {rating.Source}: {rating.Value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MovieDetails;
--- src/services/api.js
+++ src/services/api.js
@@ -4,21 +4,21 @@ const API_BASE_URL = 'http://www.omdbapi.com';
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 
 export const searchMovies = async (query, page = 1) => {
-    try {
-      const response = await axios.get(`${API_BASE_URL}/?apikey=${API_KEY}&s=${query}&page=${page}`);
-      return response.data;
-    } catch (error) {
-      console.error('Error searching movies:', error);
-      throw error;
-    }
-  };
-  
-  export const getMovieDetails = async (imdbID) => {
-    try {
-      const response = await axios.get(`${API_BASE_URL}/?apikey=${API_KEY}&i=${imdbID}`);
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching movie details:', error);
-      throw error;
-    }
-  };
+  try {
+    const response = await axios.get(`${API_BASE_URL}/?apikey=${API_KEY}&s=${query}&page=${page}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error searching movies:', error);
+    throw error;
+  }
+};
+
+export const getMovieDetails = async (imdbID) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/?apikey=${API_KEY}&i=${imdbID}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching movie details:', error);
+    throw error;
+  }
+};
