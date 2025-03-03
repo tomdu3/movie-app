e0048125935b09774a89480a9d28467f16c73204
Tomislav Dukez <tomdu3@ymail.com>
2025-03-03 05:54:16 +0000

    add Navbar and Footer to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import { BrowserRouter, Routes, Route, useLocation, useNavigate } from "react-ro
 import "./App.css";
 import Search from "./components/Search";
 import MovieDetails from "./components/MovieDetails";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <h1>Tom's Movie Database</h1>
       <Routes>
         <Route path="/" element={<Search />} />
         <Route path="/movie/:imdbID" element={<MovieDetailsWithGoBack />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
