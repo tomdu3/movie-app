43bb18c22cb7da33f8611e5fd8f827c2821799ac
Tomislav Dukez <tomdu3@ymail.com>
2025-03-03 05:52:07 +0000

    add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-white text-2xl font-bold">
+          Tom's Movie DB
+        </Link>
+        <div className="space-x-4">
+          <Link to="/" className="text-white hover:text-gray-400">
+            Home
+          </Link>
+          <Link to="/about" className="text-white hover:text-gray-400">
+            About
+          </Link>
+          <Link to="/contact" className="text-white hover:text-gray-400">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
